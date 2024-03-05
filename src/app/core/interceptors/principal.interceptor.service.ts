02c6407d5742d80deecf86b66d69577f719ff46b
MS-Marcks <gersonramos46@gmail.com>
2024-03-05 13:22:44 -0600

    feat: create interceptor and interceptor service

---
+++ src/app/core/interceptors/principal.interceptor.service.ts
@@ -0,0 +1,18 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrincipalInterceptorService implements HttpInterceptor {
+
+  constructor() { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const clone = req.clone({
+      setHeaders: {}
+    });
+    return next.handle(clone);
+  }
+
+}

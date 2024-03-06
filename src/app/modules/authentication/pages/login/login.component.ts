eceb6f004f1233c69a8d1b746a136f31461028c7
MS-Marcks <gersonramos46@gmail.com>
2024-03-06 08:14:33 -0600

    feat(AuthenticationModule): implements service in login component and create interfaces

--- src/app/modules/authentication/pages/login/login.component.ts
+++ src/app/modules/authentication/pages/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LoginUserService } from 'src/app/core/services/authentication/login-user.service';
+import { LoginUser } from 'src/app/shared/interfaces/login-user';
+import { GetFormValidationErrors } from 'src/app/shared/utils/get-form-validation-errors';
 
 @Component({
   selector: 'auth-login',
@@ -21,46 +24,57 @@ export class LoginComponent {
     }
   }
 
-  constructor(private fb: FormBuilder) {
+  alert = {
+    type: "error",
+    description: "",
+    show: false,
+    autoClose: true,
+    closeTime: 5000
+  }
+
+  constructor(private fb: FormBuilder, private service: LoginUserService) {
     this.buildForm();
   }
 
   buildForm(): void {
     this.loginForm = this.fb.group({
       email: [null, [Validators.required, Validators.email]],
-      password: [null, [Validators.required, Validators.minLength(8)]]
+      password: [null, [Validators.required]]
     });
   }
 
   async onSubmit(): Promise<void> {
-    this.getFormValidationErrors();
+    GetFormValidationErrors.Errors(this.loginForm, this.states);
 
     if (!this.loginForm.valid) {
       this.loginForm.markAllAsTouched();
       return;
     }
 
-    console.log("correct");
-  }
-
-  getFormValidationErrors() {
-    Object.keys(this.loginForm.controls).forEach(key => {
-      const controlErrors: any = this.loginForm.get(key)?.errors;
-      Object.keys(this.states[key]).forEach(keyError => {
-        if (keyError === "state") {
-          this.states[key].state = "";
-        } else {
-          this.states[key][keyError] = false;
-        }
-      });
-      if (controlErrors === null) {
+    try {
+      const body: LoginUser = {
+        username: this.loginForm.value.email,
+        password: this.loginForm.value.password,
+      }
+      const response = await this.service.login(body);
+      this.service.saveToken(response);
+    } catch (error: any) {
+      if (error.status === 401) {
+        this.setValueAlert("error", error.error.message);
         return;
       }
-      Object.keys(controlErrors).forEach(keyError => {
-        this.states[key].state = "error";
-        this.states[key][keyError] = true;
-      });
-    });
+      this.setValueAlert("error", "Ops... Ocurrio un problema");
+    }
+  }
+
+  setValueAlert(alertType: string, message: string): void {
+    this.alert.type = alertType;
+    this.alert.description = message;
+    this.alert.show = true;
+  }
+
+  handleClickCloseEvent() {
+    this.alert.show = false;
   }
 
 }

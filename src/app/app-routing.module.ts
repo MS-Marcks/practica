16872dbefaaed8f03c6f5a6f5a6c3e1f8e80b04a
MS-Marcks <gersonramos46@gmail.com>
2024-03-06 15:54:22 -0600

    feat(LibraryModule): create library module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: "", redirectTo: "auth", pathMatch: "full" },
   { path: "auth", loadChildren: () => import("./modules/authentication/authentication.module").then(m => m.AuthenticationModule) },
+  { path: "admin", loadChildren: () => import("./modules/library/library.module").then(m => m.LibraryModule) },
   { path: "**", redirectTo: "auth", pathMatch: "full" }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

dafffa8c049e0319cec55224123c5d1d51f354e2
MS-Marcks <gersonramos46@gmail.com>
2024-03-07 07:17:39 -0600

    fix(AuthenticationModule): fix control categories interest

---
+++ src/app/configs/category-interest.ts
@@ -0,0 +1,7 @@
+export const categoriesInterest = [
+  { value: false, label: "Anime" },
+  { value: false, label: "Ciencia Ficción" },
+  { value: false, label: "Novelas" },
+  { value: false, label: "Drama" },
+  { value: false, label: "Inteligencía artificial" }
+];

65cf529a8f6e1c2554091f1f9688bd1b1d5d4214
MS-Marcks <gersonramos46@gmail.com>
2024-03-08 13:32:56 -0600

    fix(UnitsTest): change configuration about jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   moduleNameMapper: {
     '@core/(.*)': '<rootDir>/src/app/core/$1',
+    '@pichincha/ds-core/(.*)': '<rootDir>/node_modules/@pichincha/ds-core/dist/ds-core/index.esm.js',
   },
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
